9cd51a87784e8fdda552968e984da4aae2b7e552
renan.lisboa <renanllisboa@gmail.com>
2023-09-26 14:18:19 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="math_package_test",
+    version='0.0.1',
+    author='Renan',
+    description='Test package',
+    packages=['math_package_test']
+)
