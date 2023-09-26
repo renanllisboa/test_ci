81b1d89ed849f158b5252020b21d6ced7e127199
renan.lisboa <renanllisboa@gmail.com>
2023-09-26 17:42:38 -0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="math_package_test",
-    version='0.0.3',
+    version='0.0.4',
     author='Renan',
     description='Test package',
     packages=['math_package_test']
